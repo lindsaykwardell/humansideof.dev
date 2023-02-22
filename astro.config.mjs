6b08db9e464c5b6af1216b0f27a9303d7894a3d9
Lindsay Wardell <three060@gmail.com>
2023-02-22 06:44:40 -0800

    File formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -10,5 +10,5 @@ import netlify from "@astrojs/netlify/functions";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
