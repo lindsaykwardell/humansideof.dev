37301265de52446c49f8dfd8edfb4f780b41cce9
Lindsay Wardell <three060@gmail.com>
2023-03-15 19:02:01 +0000

    Add page for favorite languages

---
+++ src/api/favorites.ts
@@ -0,0 +1,38 @@
+interface Language {
+    label: string
+    episodes: {
+        name: string
+        url: string
+    }[]
+}
+
+const favorites: {[key: string]: Language} = {
+    lua: {
+        label: "Lua",
+        episodes: [{
+            name: "Luiz Oliveira",
+            url: 'https://humansideof.dev/episode/1---supporting-junior-devs-and-social-media-with-luiz-oliveira'
+        }]
+    },
+    css: {
+        label: "CSS",
+        episodes: [{
+            name: "Alex Anderson",
+            url: 'https://humansideof.dev/episode/2---technical-evaluation-and-space-simulators-with-alex-anderson'
+        }]
+    },
+    ts: {
+        label: "Typescript",
+        episodes: [{
+            name: "Star Richardson",
+            url: "https://humansideof.dev/episode/3---learning-to-learn-and-code-with-star-richardson"
+        },
+        {
+            name: "Daniel Roe",
+            url: "https://humansideof.dev/episode/4---exploring-open-source-with-daniel-roe"
+        }
+    ]
+    }
+}
+
+export default favorites
