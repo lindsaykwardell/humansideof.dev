bbdd0329b0beb7763a5e9135b08c73b5a3c48ed1
Lindsay Wardell <three060@gmail.com>
2023-03-26 22:00:47 -0700

    Add episode 5 favorite language

--- src/api/favorites.ts
+++ src/api/favorites.ts
@@ -1,38 +1,52 @@
 interface Language {
-    label: string
-    episodes: {
-        name: string
-        url: string
-    }[]
+  label: string;
+  episodes: {
+    name: string;
+    url: string;
+  }[];
 }
 
-const favorites: {[key: string]: Language} = {
-    lua: {
-        label: "Lua",
-        episodes: [{
-            name: "Luiz Oliveira",
-            url: 'https://humansideof.dev/episode/1---supporting-junior-devs-and-social-media-with-luiz-oliveira'
-        }]
-    },
-    css: {
-        label: "CSS",
-        episodes: [{
-            name: "Alex Anderson",
-            url: 'https://humansideof.dev/episode/2---technical-evaluation-and-space-simulators-with-alex-anderson'
-        }]
-    },
-    ts: {
-        label: "Typescript",
-        episodes: [{
-            name: "Star Richardson",
-            url: "https://humansideof.dev/episode/3---learning-to-learn-and-code-with-star-richardson"
-        },
-        {
-            name: "Daniel Roe",
-            url: "https://humansideof.dev/episode/4---exploring-open-source-with-daniel-roe"
-        }
-    ]
-    }
-}
+const favorites: { [key: string]: Language } = {
+  lua: {
+    label: "Lua",
+    episodes: [
+      {
+        name: "Luiz Oliveira",
+        url: "https://humansideof.dev/episode/1---supporting-junior-devs-and-social-media-with-luiz-oliveira",
+      },
+    ],
+  },
+  css: {
+    label: "CSS",
+    episodes: [
+      {
+        name: "Alex Anderson",
+        url: "https://humansideof.dev/episode/2---technical-evaluation-and-space-simulators-with-alex-anderson",
+      },
+    ],
+  },
+  ts: {
+    label: "Typescript",
+    episodes: [
+      {
+        name: "Star Richardson",
+        url: "https://humansideof.dev/episode/3---learning-to-learn-and-code-with-star-richardson",
+      },
+      {
+        name: "Daniel Roe",
+        url: "https://humansideof.dev/episode/4---exploring-open-source-with-daniel-roe",
+      },
+    ],
+  },
+  html: {
+    label: "HTML",
+    episodes: [
+      {
+        name: "Ben Myers",
+        url: "https://humansideof.dev/episode/5---documentation-and-accessibility-with-ben-myers",
+      },
+    ],
+  },
+};
 
-export default favorites
+export default favorites;
