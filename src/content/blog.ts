0fde8333c293d12fc617aa26d89e3d3fe59fe532
Lindsay Wardell <three060@gmail.com>
2024-06-11 18:25:44 -0700

    cleanup

---
+++ src/content/blog.ts
@@ -0,0 +1,15 @@
+import { getCollection } from "astro:content";
+import dayjs from "dayjs";
+
+export async function allPosts(limit?: number) {
+  const posts = await getCollection(
+    "blog",
+    (post) => post.data.published === true || import.meta.env.DEV
+  );
+
+  return [...posts]
+    .sort((a, b) =>
+      dayjs(a.data.pubDate).isBefore(dayjs(b.data.pubDate)) ? 1 : -1
+    )
+    .splice(0, limit);
+}
---
+++ src/content/episode.ts
@@ -0,0 +1,40 @@
+import { extract } from "@extractus/feed-extractor";
+
+interface Feed {
+  title: string;
+  description: string;
+  entries: {
+    title: string;
+    published: string;
+    enclosure: {
+      url: string;
+    };
+    description: string;
+    summary: string;
+  }[];
+}
+
+export const getEpisodes = (limit?: number) =>
+  (
+    extract("https://anchor.fm/s/81f880f0/podcast/rss", {
+      getExtraEntryFields: (feedEntry: any) => {
+        const { enclosure } = feedEntry;
+        return {
+          enclosure: {
+            url: enclosure["@_url"],
+            type: enclosure["@_type"],
+            length: enclosure["@_length"],
+          },
+          summary: feedEntry["itunes:summary"],
+        };
+      },
+    }) as any as Promise<Feed>
+  ).then(({ entries }) => entries.slice(0, limit));
+
+export const getEpisode = (slug: string) =>
+  getEpisodes().then((entries) =>
+    entries.find(
+      (ep: { title: string }) =>
+        ep.title.toLowerCase().replaceAll(" ", "-") === slug
+    )
+  );
