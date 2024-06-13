60c3d2b6261f40cc767b6e623ad71011b9901221
Lindsay Wardell <three060@gmail.com>
2024-06-13 14:46:57 -0700

    Tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
