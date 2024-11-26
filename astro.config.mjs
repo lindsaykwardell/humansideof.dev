7b40790f13c2bf93c8174f11b3c9f48a0446fa3b
Lindsay Wardell <three060@gmail.com>
2024-11-26 23:07:49 +0000

    Add Bluesky comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,13 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import netlify from "@astrojs/netlify";
 
+import react from "@astrojs/react";
+
+import simpleStackQuery from "simple-stack-query";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react(), simpleStackQuery()],
   output: "server",
   adapter: netlify(),
 });
